c58e504bff09d6161c114475dbb0d4a8f6daa885
ivan.knoefler <ivan.knoefler@ost.ch>
2025-05-22 22:06:16 +0200

    move harpoon keymaps

--- .config/nvim/lua/iknoefler/core/keymaps.lua
+++ .config/nvim/lua/iknoefler/core/keymaps.lua
@@ -70,27 +70,6 @@ keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current c
 -- restart lsp server
 keymap.set("n", "<leader>rs", ":LspRestart<CR>")
 
--- harpoon
-local harpoon_mark = require("harpoon.mark")
-local harpoon_ui = require("harpoon.ui")
-
--- Harpoon keymaps
-keymap.set("n", "<leader>a", harpoon_mark.add_file, { desc = "Harpoon: Add file" })
-keymap.set("n", "<leader>j", harpoon_ui.toggle_quick_menu, { desc = "Harpoon: Toggle menu" })
-
-keymap.set("n", "<C-x>", function()
-  harpoon_ui.nav_file(1)
-end, { desc = "Harpoon: Go to file 1" })
-keymap.set("n", "<C-n>", function()
-  harpoon_ui.nav_file(2)
-end, { desc = "Harpoon: Go to file 2" })
-keymap.set("n", "<C-s>", function()
-  harpoon_ui.nav_file(3)
-end, { desc = "Harpoon: Go to file 3" })
-keymap.set("n", "<C-m>", function()
-  harpoon_ui.nav_file(4)
-end, { desc = "Harpoon: Go to file 4" })
-
 -- Snacks
 keymap.set("n", "<leader>lg", function()
   Snacks.lazygit()
--- .config/nvim/lua/iknoefler/plugins/harpoon.lua
+++ .config/nvim/lua/iknoefler/plugins/harpoon.lua
@@ -1,6 +1,26 @@
 return {
   "ThePrimeagen/harpoon",
   config = function()
-    require("harpoon").setup()
+    local mark = require("harpoon.mark")
+    local ui = require("harpoon.ui")
+
+    vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: Add file" })
+    vim.keymap.set("n", "<leader>j", ui.toggle_quick_menu, { desc = "Harpoon: Toggle menu" })
+
+    vim.keymap.set("n", "<C-x>", function()
+      ui.nav_file(1)
+    end)
+
+    vim.keymap.set("n", "<C-n>", function()
+      ui.nav_file(2)
+    end)
+
+    vim.keymap.set("n", "<C-s>", function()
+      ui.nav_file(3)
+    end)
+
+    vim.keymap.set("n", "<C-m>", function()
+      ui.nav_file(4)
+    end)
   end,
 }
